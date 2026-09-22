addc64c7f2483a5944147428b92e42995bf03917
Mark Alexis Jaudian Cabrera <solesourc3@gmail.com>
2026-09-23 01:18:16 +0800

    asdas

--- home.js
+++ home.js
@@ -225,7 +225,6 @@
         btn.className = 'episode-btn';
         btn.href = ep.link;
         btn.target = '_blank';
-        btn.rel = 'noopener noreferrer';
         btn.setAttribute('aria-label', `Watch ${ep.title} (Tagalog)`);
         btn.innerHTML = `
             <svg viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>
